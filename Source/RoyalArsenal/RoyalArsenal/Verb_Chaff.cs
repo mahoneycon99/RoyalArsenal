c0db69b977fcf82532f05fafa38d6a12b090db6d
mahoneycon99 <mahoneycon99@gmail.com>
2021-07-31 07:01:19 -0400

    Added melee weapons - mono-greatsword, plasma greatsword Added ranged weapons - royal charge rifle, royal charge lance, mono-longbow Added armors - ranseur, defender, phalanx

---
+++ Source/RoyalArsenal/RoyalArsenal/Verb_Chaff.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace RoyalArsenal
+{
+    public class Verb_Chaff : Verb
+    {
+		protected override bool TryCastShot()
+		{
+			Pop(base.ReloadableCompSource);
+			return true;
+		}
+
+		public override float HighlightFieldRadiusAroundTarget(out bool needLOSToCenter)
+		{
+			needLOSToCenter = false;
+			return base.EquipmentSource.GetStatValue(StatDefOf.SmokepopBeltRadius);
+		}
+
+		public override void DrawHighlight(LocalTargetInfo target)
+		{
+			DrawHighlightFieldRadiusAroundTarget(caster);
+		}
+
+		public static void Pop(CompReloadable comp)
+		{
+			if (comp != null && comp.CanBeUsed)
+			{
+				ThingWithComps parent = comp.parent;
+				Pawn wearer = comp.Wearer;
+				GenExplosion.DoExplosion(wearer.Position, wearer.Map, parent.GetStatValue(StatDefOf.SmokepopBeltRadius), DamageDefOf.Smoke, null, -1, -1f, null, null, null, null, ThingDef.Named("RA_Chaff"), 1f);
+				comp.UsedOnce();
+			}
+		}
+	}
+}
---
+++ Source/RoyalArsenal/RoyalArsenal/Verb_Cloak.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace RoyalArsenal
+{
+    class Verb_Cloak : Verb
+    {
+        private CompProperties_AbilityGiveHediff giveHediffProps;
+        private float DURATION = 30.0f;
+        protected override bool TryCastShot()
+        {
+            if (ReloadableCompSource != null && currentTarget != null)
+            {
+                giveHediffProps = new CompProperties_AbilityGiveHediff();
+                giveHediffProps.hediffDef = HediffDef.Named("RA_CloakingField");
+                giveHediffProps.applyToSelf = true;
+                giveHediffProps.onlyApplyToSelf = true;
+                giveHediffProps.replaceExisting = true;
+
+                ApplyCloak(currentTarget.Pawn, ReloadableCompSource);
+
+                ReloadableCompSource.UsedOnce();
+            }
+            return true;
+        }
+
+        private void ApplyCloak(Pawn target, CompReloadable comp)
+        {
+            if (target == null)
+            {
+                return;
+            }
+            if (giveHediffProps.replaceExisting)
+            {
+                Hediff firstHediffOfDef = target.health.hediffSet.GetFirstHediffOfDef(giveHediffProps.hediffDef);
+                if (firstHediffOfDef != null)
+                {
+                    target.health.RemoveHediff(firstHediffOfDef);
+                }
+            }
+            Hediff hediff = HediffMaker.MakeHediff(giveHediffProps.hediffDef, target, giveHediffProps.onlyBrain ? target.health.hediffSet.GetBrain() : null);
+            HediffComp_Disappears hediffComp_Disappears = hediff.TryGetComp<HediffComp_Disappears>();
+            if (hediffComp_Disappears != null)
+            {
+                hediffComp_Disappears.ticksToDisappear = GenTicks.SecondsToTicks(DURATION);
+            }
+            HediffComp_Link hediffComp_Link = hediff.TryGetComp<HediffComp_Link>();
+            if (hediffComp_Link != null)
+            {
+                hediffComp_Link.other = target;
+                //hediffComp_Link.drawConnection = target == parent.pawn;
+            }
+            target.health.AddHediff(hediff);
+
+            ThingWithComps parent = comp.parent;
+            Pawn wearer = comp.Wearer;
+            GenExplosion.DoExplosion(wearer.Position, wearer.Map, 2.9f, DamageDefOf.Smoke, null, -1, -1f, null, null, null, null, null, 1f);
+        }
+    }
+}
